8c4118b254378f463d3fb31ca3b081906bcdc22e
Maksim Mysak <maksimmysak@gmail.com>
2019-03-26 00:17:55 +0200

    add base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Routing } from './app.routing';
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    Routing
   ],
   providers: [],
   bootstrap: [AppComponent]
